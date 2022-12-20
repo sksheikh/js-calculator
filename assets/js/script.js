20fdbb132de4c3ccfc53574ad1837d26b2f5d402
sheikh <skuddin.ius@gmail.com>
2022-12-20 15:06:57 +0600

    simple calculator with js

---
+++ assets/js/script.js
@@ -0,0 +1,45 @@
+
+function calculator(operator){
+    var firstNumber = Number(document.getElementById('firstNumber').value);
+    var lastNumber = Number(document.getElementById('lastNumber').value);
+    var result = 0;
+
+    switch(operator){
+        case '+' : result = firstNumber + lastNumber;
+        break;
+        case '-' : result = firstNumber - lastNumber;
+        break;
+        case '*' : result = firstNumber * lastNumber;
+        break;
+        case '/' : result = firstNumber / lastNumber;
+        break;
+
+    }
+    document.getElementById('result').value = result;
+    
+}
+
+var divi = document.getElementById('divi');
+divi.onclick = function(){
+    calculator('/')
+}
+var multi = document.getElementById('multi');
+multi.onclick = function(){
+    calculator('*')
+}
+var sub = document.getElementById('sub');
+sub.onclick = function(){
+    calculator('-')
+}
+
+var addi = document.getElementById('addi');
+addi.onclick = function(){
+    calculator('+')
+}
+
+var reset = document.getElementById('reset');
+reset.onclick = function(){
+    document.getElementById('firstNumber').value = '';
+    document.getElementById('lastNumber').value = '';
+    document.getElementById('result').value = '';
+}
