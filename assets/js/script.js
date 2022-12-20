f97dab8ae4eb334146d305c6745b3a66436245bc
sheikh <skuddin.ius@gmail.com>
2022-12-20 15:28:50 +0600

    get result button add for getting result

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,27 +19,34 @@ function calculator(operator){
     
 }
 
-var divi = document.getElementById('divi');
-divi.onclick = function(){
-    calculator('/')
-}
-var multi = document.getElementById('multi');
-multi.onclick = function(){
-    calculator('*')
-}
-var sub = document.getElementById('sub');
-sub.onclick = function(){
-    calculator('-')
-}
+// var divi = document.getElementById('divi');
+// divi.onclick = function(){
+//     calculator('/')
+// }
+// var multi = document.getElementById('multi');
+// multi.onclick = function(){
+//     calculator('*')
+// }
+// var sub = document.getElementById('sub');
+// sub.onclick = function(){
+//     calculator('-')
+// }
 
-var addi = document.getElementById('addi');
-addi.onclick = function(){
-    calculator('+')
-}
+// var addi = document.getElementById('addi');
+// addi.onclick = function(){
+//     calculator('+')
+// }
 
 var reset = document.getElementById('reset');
 reset.onclick = function(){
     document.getElementById('firstNumber').value = '';
     document.getElementById('lastNumber').value = '';
     document.getElementById('result').value = '';
+}
+
+
+var getResult = document.getElementById('getResult');
+getResult.onclick = function(){
+    var operator = document.querySelector('input[name="operator"]:checked').value;
+    calculator(operator);
 }
